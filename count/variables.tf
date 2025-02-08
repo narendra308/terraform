9ea6ee5186f6b33426e2a2f003e344afd8839cb2
narendramogili <narendramogili308@gmail.com>
2025-02-08 15:43:05 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instance" {
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+    default = "Z02824211YKBRXEJQ03P6"
+}
+
+variable "domain_name" {
+    default = "mogili.online"
+}
